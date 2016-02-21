f685f5ec83730c97f09b104b90d462ec74705edd
Ogun Heper <ogun.heper@bilyoner.com>
2016-02-21 14:18:35 +0200

    Eureka added to hystrix and turbine

--- turbine/src/main/java/com/ogunheper/microservices/TurbineApplication.java
+++ turbine/src/main/java/com/ogunheper/microservices/TurbineApplication.java
@@ -2,8 +2,10 @@ package com.ogunheper.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.amqp.EnableTurbineAmqp;
 
+@EnableDiscoveryClient
 @EnableTurbineAmqp
 @SpringBootApplication
 public class TurbineApplication {
